34ff90ea731d95293b17c2a42cd0a8dc65fcf2d1
daudangtung1 <dangtung.dau.bka@gmail.com>
2020-11-19 10:05:57 +0700

    add fe

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,31 @@
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="EnvyTheme">
+    <meta name="keywords" content="academy, classes, college, course, education, education template, elearning, institute, instructor, learning, Online Learning, study, teaching, training, university">
+    <meta name="description" content="EduField is a Responsive and Creative HTML template for education niche website! This template is suitable for offering learning courses, sharing workshops or events, publishing education institute and many more on the web">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <!-- IcoFont Min CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/icofont.min.css')}}">
+    <!-- Classy Nav CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/classy-nav.min.css')}}">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
+    <!-- Owl Carousel CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/owl.carousel.css')}}">
+    <!-- Magnific Popup CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
+    <!-- Owl Theme Default CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/owl.theme.default.min.css')}}">
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}">
+
+    <title>EduField - Education & Online Courses HTML Template</title>
+
+    <link rel="icon" type="image/png" href="{{asset('assets/img/favicon.png')}}">
+</head>
